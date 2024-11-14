6a19b3f28e21c5f8e7f97869f1fcd9077a767f2f
Ian <mancnix@protonmail.com>
2024-11-14 19:19:37 +0000

    added basic tests for getBalance

--- accounts.js
+++ accounts.js
@@ -3,10 +3,23 @@ const db = { "Barry": 30, "Steve": 0 }
 
 function checkBalance( username ) {
   // return customer balance
-  return db[username]
+  const balance = db[username]
+
+  if (balance == undefined) {
+    console.log("Unknown user")
+    return undefined
+  }
+
+  return balance
 }
 
 
-console.assert(checkBalance("Barry") == 30, "You effed up")
-console.assert(checkBalance("Alice") == undefined, "You effed up")
 
+console.log("Get barry's balance")
+console.assert(checkBalance("Barry") == 30, "You effed up, didn't get Barry's amount")
+
+console.log("Get steve's balance")
+console.assert(checkBalance("Steve") == 0, "You effed up")
+
+console.log("Try and get Alice's balance, but she has not account")
+console.assert(checkBalance("Alice") == undefined, "You effed up")
